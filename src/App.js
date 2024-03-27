29b9e73785f1efc16a0b515e84fbc3a3aef77b7a
vivekdobariya <vivekdobariya333@gmail.com>
2024-03-27 16:36:02 +0530

    add route and solve error

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import "./App.css";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import React, { useState } from 'react';
+import Alert from "./components/Alert";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
+function App() {
+  const [mode, setmode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const togglemode = () => {
+    if (mode === "light") {
+      setmode("dark")
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else {
+      setmode("light")
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
+  return (
+    <>
+      <Router>
+        <Navbar title="TextUtils" aboutText="About" mode={mode} togglemode={togglemode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/Textutils-React"
+              element={<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />}>
+            </Route>
+            <Route exact path="/Textutils-React/about"
+              element={<About mode={mode} />}>
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
