bdb7896ae59725785556417fcb1e98c910826443
vivekdobariya <vivekdobariya333@gmail.com>
2024-03-27 15:40:17 +0530

    remove log code

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -49,7 +49,6 @@ export default function TextForm(props) {
                 <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
                 <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>         
             </div>
-            {console.log(text.split(/\s+/).filter((element) => { return element.length !== 0 }).length)}    
             <div className="container my-3" style={{ color: props.mode === "dark" ? "white" : "#042743" }}>
                 <h2>Your text summery</h2>
                 <p>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words and {text.length} characters</p>
